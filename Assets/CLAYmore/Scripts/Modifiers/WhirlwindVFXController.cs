5be1dd04e38f59ffd6baf48894efe2b63b2bd65a
razgulchik <39990779+razgulchik@users.noreply.github.com>
2026-05-15 10:49:22 +0200

    Вихрь доделал

--- Assets/CLAYmore/Scripts/Modifiers/WhirlwindVFXController.cs
+++ Assets/CLAYmore/Scripts/Modifiers/WhirlwindVFXController.cs
@@ -1,29 +1,29 @@
-using CLAYmore.ECS;
-using UnityEngine;
+// using CLAYmore.ECS;
+// using UnityEngine;
 
-namespace CLAYmore
-{
-    public class WhirlwindVFXController : MonoBehaviour
-    {
-        [SerializeField] private PrefabPool vfxPool;
+// namespace CLAYmore
+// {
+//     public class WhirlwindVFXController : MonoBehaviour
+//     {
+//         [SerializeField] private PrefabPool vfxPool;
 
-        private void OnEnable()
-            => World.Current?.Events.Subscribe<WhirlwindActivatedEvent>(OnWhirlwindActivated);
+//         private void OnEnable()
+//             => World.Current?.Events.Subscribe<WhirlwindActivatedEvent>(OnWhirlwindActivated);
 
-        private void OnDisable()
-            => World.Current?.Events.Unsubscribe<WhirlwindActivatedEvent>(OnWhirlwindActivated);
+//         private void OnDisable()
+//             => World.Current?.Events.Unsubscribe<WhirlwindActivatedEvent>(OnWhirlwindActivated);
 
-        private void OnWhirlwindActivated(WhirlwindActivatedEvent evt)
-        {
-            if (vfxPool == null) return;
-            GameObject vfx = vfxPool.Get(evt.WorldPosition);
+//         private void OnWhirlwindActivated(WhirlwindActivatedEvent evt)
+//         {
+//             if (vfxPool == null) return;
+//             GameObject vfx = vfxPool.Get(evt.WorldPosition);
 
-            var bridge = vfx.GetComponent<VFXBridge>();
-            if (bridge != null)
-            {
-                bridge.OnImpact   = () => World.Current?.Events.Publish(new WhirlwindDetonateEvent { Cell = evt.Cell });
-                bridge.OnComplete = () => vfxPool.Return(vfx);
-            }
-        }
-    }
-}
+//             var bridge = vfx.GetComponent<VFXBridge>();
+//             if (bridge != null)
+//             {
+//                 bridge.OnImpact   = () => World.Current?.Events.Publish(new WhirlwindDetonateEvent { Cell = evt.Cell });
+//                 bridge.OnComplete = () => vfxPool.Return(vfx);
+//             }
+//         }
+//     }
+// }
